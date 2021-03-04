c7b35ddababe1e77350321c927b4df899fcf0171
Michael Schnupp <michael.schnupp@accenture.com>
2021-03-04 02:28:58 +0100

    change example data

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -eux
-< /dev/urandom base64 | head -n 1000000 >random
-< random base64 | gzip >random.64.gz
 python3 -m http.server 8001 &
 xdg-open http://localhost:8001/base.html
 
